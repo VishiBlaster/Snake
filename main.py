9ab1a0bbe81d790e85921746c20242b2b65549a1
VishiBlaster <93249723+VishiBlaster@users.noreply.github.com>
2022-06-27 10:40:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random
-player = input("Choose Snake, Water Or Gun")
+player = int(input("Choose Snake, Water Or Gun /n By entering 0, for Snake 1 for Water and 2 for Gun" ))
 computer = random.randrange(0, 2)
 if computer == 0():
-  print("Computer Choose Snake")
